88d9b1265e610220c919ab0ed539f9f94589f508
Dom\Ivan <nt-polza@yandex.ru>
2021-05-28 13:52:46 +0500

    Чистка HTML от лишних атрибутов

--- hilda.js
+++ hilda.js
@@ -5,6 +5,25 @@ titleSkills = Array.from(titleSkills);
 //console.log(titleSkills);
 
 let skills = document.querySelectorAll(".elementor-tab-content.elementor-clearfix");
+//Чистим HTML от лишних блоков и атрибутов
+for (let input of skills) {
+
+  let p = input.querySelectorAll('p');
+  p.forEach((item, i) => {
+    item.removeAttribute("class");
+    item.removeAttribute("style");
+  });
+
+  let figure = input.querySelectorAll('figure');
+  figure.forEach((item, i) => {
+    item.removeAttribute("class");
+  });
+
+  n = input.querySelectorAll('div');
+  if (n.length>0) input.querySelector('div').remove();
+
+}
+
 skills = Array.from(skills);
 //console.log(skills);
 
@@ -19,5 +38,7 @@ hilda.skills = [];
 skills.forEach((item, i) => {
   hilda.skills[i] = item.innerHTML;
 });
-
 console.log(hilda);
+
+let hero1 = JSON.stringify(hilda);
+//console.log(hero1);
